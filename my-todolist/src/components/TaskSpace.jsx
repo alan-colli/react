45e0d92f4021679763a199c963457118a70516a2
Alan Colli <150622900+alan-colli@users.noreply.github.com>
2024-05-26 13:00:45 -0300

    att

--- my-todolist/src/components/TaskSpace.jsx
+++ my-todolist/src/components/TaskSpace.jsx
@@ -2,13 +2,13 @@ import NoTaskCreated from "./NoTaskCreated";
 
 export default function TaskSpace({ tasks }) {
   return (
-    <div className="mt-24 flex flex-col items-center justify-center bg-blue-950 text-white text-2xl h-96 mx-24 border-4 border-blue-400 rounded-3xl">
+    <div className="mt-24 flex inlineflex items-center justify-center bg-blue-950 text-white text-2xl h-96 mx-24 border-4 border-blue-400 rounded-3xl  ">
       {tasks.length === 0 && <NoTaskCreated />}
       {tasks.map((task) => (
-        <div>
-          <p>{task.Title}</p>
-          <p>{task.Task}</p>
-          <p>{task.DueDate}</p>
+        <div className="bg-blue-400 p-4 border-4 border-black rounded-3xl text-black h-40 mx-4 flex flex-col justify-center h-56 w-38">
+          <p>Title: {task.Title}</p>
+          <p>Task: {task.Task}</p>
+          <p>Due Date: {task.DueDate}</p>
         </div>
       ))}
     </div>
