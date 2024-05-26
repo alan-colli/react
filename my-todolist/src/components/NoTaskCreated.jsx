7b56a79ba64c37f1ae3a7ac427c6f166cae9f726
Alan Colli <150622900+alan-colli@users.noreply.github.com>
2024-05-26 12:46:56 -0300

    att

--- my-todolist/src/components/NoTaskCreated.jsx
+++ my-todolist/src/components/NoTaskCreated.jsx
@@ -1,6 +1,6 @@
 export default function NoTaskCreated() {
   return (
-    <div className="bg-blue-400 w-96 flex flex-col justify-center items-center rounded-md border-2 border-blue-900">
+    <div className="bg-blue-400 w-96 text-center rounded-md border-2 border-blue-900">
       <h2>None task have been added!</h2>
       <p>Create a new task pressing the button "+ Add"</p>
     </div>
--- my-todolist/src/components/TaskSpace.jsx
+++ my-todolist/src/components/TaskSpace.jsx
@@ -2,7 +2,7 @@ import NoTaskCreated from "./NoTaskCreated";
 
 export default function TaskSpace({ tasks }) {
   return (
-    <div className="mt-24 flex flex-col items-center  bg-blue-950 text-white text-2xl h-96 mx-24 border-4 border-blue-400 rounded-3xl">
+    <div className="mt-24 flex flex-col items-center justify-center bg-blue-950 text-white text-2xl h-96 mx-24 border-4 border-blue-400 rounded-3xl">
       {tasks.length === 0 && <NoTaskCreated />}
       {tasks.map((task) => (
         <div>
