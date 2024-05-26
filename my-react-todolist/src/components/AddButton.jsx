537b93043993eefb7ce3b02a6ae676a089705a3f
Alan Colli <150622900+alan-colli@users.noreply.github.com>
2024-05-26 12:20:42 -0300

    Delete my-react-todolist directory

--- my-react-todolist/src/components/AddButton.jsx
+++
@@ -1,9 +0,0 @@
-export default function Button({ children }) {
-  return (
-    <div className=" flex flex-col justify-center items-center">
-      <button className=" text-white bg-blue-600 hover:bg-blue-400 w-24 h-22 border-2 border-black rounded-md">
-        {children}
-      </button>
-    </div>
-  );
-}
--- my-react-todolist/src/components/TaskSpace.jsx
+++
@@ -1,7 +0,0 @@
-export default function ProjectSpace() {
-  return (
-    <div className="mt-24 flex flex-col items-center  bg-blue-950 text-white text-2xl h-96 mx-24 border-4 border-blue-400 rounded-3xl">
-      <h2>Your tasks : </h2>
-    </div>
-  );
-}
