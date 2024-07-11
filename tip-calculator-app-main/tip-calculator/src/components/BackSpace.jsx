8032f96e0f826d5d906fabef639b0ee25c9e676a
Alan Colli <150622900+alan-colli@users.noreply.github.com>
2024-07-11 17:24:06 -0300

    att

--- tip-calculator-app-main/tip-calculator/src/components/BackSpace.jsx
+++ tip-calculator-app-main/tip-calculator/src/components/BackSpace.jsx
@@ -1,51 +1,59 @@
+import React from "react";
+import Results from "./Results";
+
 export default function BackSpace() {
   return (
     <div className="flex flex-col justify-center items-center h-screen">
-      <div className="bg-white h-96 w-2/3 rounded-3xl jutify-start items-start p-10">
-        <div>
+      <div className="bg-white h-96 w-2/3 rounded-3xl p-10 flex">
+        <div className="w-1/2">
           <p className="pb-4">Bill</p>
-          <div>
+          <div className="relative">
+            <span className="absolute right-64 inset-y-2 text-black">$</span>
             <input
               type="number"
               name="price"
               id="price"
-              className="block rounded-md border-0 py-1.5 pl-4 pr-10 text-gray-900 bg-blue-100 placeholder:text-gray-400 w-36 placeholder:text-center"
-              placeholder="$ 0.00"
+              className="block rounded-md border-0 py-1.5 pl-8 pr-4 text-gray-900 bg-blue-100 placeholder:text-gray-400 w-36 placeholder:text-center"
+              placeholder="0.00"
             />
-            <span className="absolute inset-y-0 right-3  text-gray-500">$</span>
           </div>
           <p className="pt-4 pb-4">Select tip %</p>
-          <div className="grid grid-cols-3 gap-2 w-1/2 pb-4">
-            <button className="bg-buttonNormal text-gray-100 w-28 h-8 rounded-md hover:scale-105">
+          <div className="grid grid-cols-3 gap-4 w-1/2 pb-4">
+            <button className="bg-buttonNormal text-gray-100 w-16 h-8 rounded-md hover:scale-105">
               5%
             </button>
-            <button className="bg-buttonNormal text-gray-100 w-28 h-8 rounded-md hover:scale-105">
+            <button className="bg-buttonNormal text-gray-100 w-16 h-8 rounded-md hover:scale-105">
               10%
             </button>
-            <button className="bg-buttonNormal text-gray-100 w-28 h-8 rounded-md hover:scale-105">
+            <button className="bg-buttonNormal text-gray-100 w-16 h-8 rounded-md hover:scale-105">
               15%
             </button>
-            <button className="bg-buttonNormal text-gray-100 w-28 h-8 rounded-md hover:scale-105">
+            <button className="bg-buttonNormal text-gray-100 w-16 h-8 rounded-md hover:scale-105">
               20%
             </button>
-            <button className="bg-buttonNormal text-gray-100 w-28 h-8 rounded-md hover:scale-105">
+            <button className="bg-buttonNormal text-gray-100 w-16 h-8 rounded-md hover:scale-105">
               25%
             </button>
-            <button className="bg-gray-100 text-buttonNormal w-28 h-8 rounded-md hover:scale-105">
+            <button className="bg-gray-100 text-buttonNormal w-16 h-8 rounded-md hover:scale-105">
               Custom
             </button>
           </div>
           <p className="pb-4">Amount of people</p>
-          <div>
+          <div className="relative">
+            <span className="absolute right-64 inset-y-2 text-black">
+              People
+            </span>
             <input
               type="number"
-              name="price"
-              id="price"
-              className="block rounded-md border-0 py-1.5 pl-4 pr-10 text-gray-900 bg-blue-100 placeholder:text-gray-400 w-36 placeholder:text-center"
+              name="people"
+              id="people"
+              className="block rounded-md border-0 py-1.5 pl-8 pr-4 text-gray-900 bg-blue-100 placeholder:text-gray-400 w-36 placeholder:text-center"
               placeholder="0"
             />
-            <span className="absolute  right-3 text-black">$</span>
           </div>
+        </div>
+        <div className="w-1/2 flex justify-center items-center">
+          <Results />
         </div>
       </div>
     </div>
