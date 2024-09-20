1c243f39504fa68bc997dd67653b59fa6f224802
Alan Colli <150622900+alan-colli@users.noreply.github.com>
2024-09-20 14:50:49 -0300

    Update ClickScreen.jsx

--- exercRaulReact/src/Components/ClickScreen.jsx
+++ exercRaulReact/src/Components/ClickScreen.jsx
@@ -1,15 +1,26 @@
 import React, { useState } from "react";
 
 export default function ClickScreen() {
-  const [clickPosition, setClickPosition] = useState(null);
+  const [clickPosition, setClickPosition] = useState({ x: 0, y: 0 });
+  const [position, setPosition] = useState([]);
 
-  const handleClick = (e) => {
-    const { clientX, clientY } = e;
-    setClickPosition({ x: clientX, y: clientY });
-    console.log(e);
+  const handleClick = (event) => {
+    const rect = event.target.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    setClickPosition({ x, y });
   };
 
+  const insertPosition = (position) => {
+    setPosition((prevArray) => [...prevArray, position]);
+  };
+
+
+
   return (
-    <div className="w-[60vw] h-[60vh] bg-white " onClick={handleClick}></div>
+    <div
+      className="w-[60vw] h-[60vh] bg-white "
+      onClick={}
+    ></div>
   );
 }
