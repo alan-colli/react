678b364a68d63df3ebb6b98cfd5ce6aa0a283246
Alan Colli <150622900+alan-colli@users.noreply.github.com>
2024-09-25 11:40:18 -0300

    at

--- ToDoListLocalStorage/src/App.jsx
+++ ToDoListLocalStorage/src/App.jsx
@@ -41,7 +41,7 @@ export default function App() {
             None activity has been added!
           </p>
         ) : (
-          <Card />
+          <Card task={task} />
         )}
       </div>
       <Modal
--- ToDoListLocalStorage/src/Components/Card.jsx
+++ ToDoListLocalStorage/src/Components/Card.jsx
@@ -1,10 +1,12 @@
-export default function Card({ list, onDeleteTask, onEditTask }) {
+export default function Card({ list }) {
   return (
     <div className="h-[28vh] w-[12.5vw] bg-gray-100 flex flex-col justify-start items-start rounded-md pl-4 pt-4 space-y-8">
-      <p>Activity: </p>
-      <p>asdfgh</p>
-      <p>Due Date: </p>
-      <p>12345678</p>
+      {list.map((task, index) => (
+        <div key={index}>
+          <p className="my-2">Activity: {task.Activity}</p>
+          <p className="my-2">Date: {task.Date}</p>
+        </div>
+      ))}
     </div>
   );
 }
