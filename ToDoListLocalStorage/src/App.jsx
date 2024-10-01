15fc3cb2b21c653f04da39c3265aac9b138ee42b
Alan Colli <150622900+alan-colli@users.noreply.github.com>
2024-10-01 09:00:04 -0300

    att

--- ToDoListLocalStorage/src/App.jsx
+++ ToDoListLocalStorage/src/App.jsx
@@ -5,6 +5,7 @@ import Header from "./Components/Header";
 export default function App() {
   const [showModal, setShowModal] = useState(false);
   const [list, setList] = useState([]);
+  const [edit, setEdit] = useState("");
 
   const handleModalOpen = () => {
     setShowModal(true);
@@ -14,16 +15,40 @@ export default function App() {
     setShowModal(false);
   };
 
+  function handleEdit(id) {
+    console.log(id);
+    setEdit(id);
+    handleModalOpen();
+  }
+  console.log(list);
   const handleAttList = (task) => {
-    setList((prevList) => [...prevList, task]);
-    console.log(list);
+    console.log(task);
+    console.log(edit);
+
+    if (edit) {
+      setList((prevList) => {
+        return prevList.map((_task) =>
+          _task.Number === edit
+            ? {
+                ..._task,
+                ...task,
+              }
+            : _task
+        );
+      });
+      setEdit("");
+      return;
+    }
+
+    setList((prevList) => [
+      ...prevList,
+      { ...task, Number: crypto.randomUUID() },
+    ]);
   };
 
   const handleDelete = (index) => {
     setList((prevState) => prevState.filter((_, i) => i !== index));
   };
-
-  const handleEdit = (index) => {};
 
   return (
     <div className="bg-blue-800 h-[100vh] w-[100vw] justify-start flex flex-col items-center">
@@ -35,9 +60,6 @@ export default function App() {
         >
           Add
         </button>
-        <button className="h-[2vh] w-[3vw] border-2 flex justify-center items-center bg-black text-white font-sans hover:border-purple-900  hover:text-purple-400">
-          Edit
-        </button>
       </div>
       <div className="bg-purple-900 w-[70vw] h-[80vh] flex items-start justify-start rounded-md p-8">
         {list.length === 0 ? (
@@ -47,39 +69,39 @@ export default function App() {
         ) : (
           <div className="flex flex-wrap  ">
             {list.map((task, index) => (
-              <>
-                <div className="flex flex-col justify-center items-center">
-                  <div
-                    key={index}
-                    className="ml-4 h-[28vh] w-[12.5vw] bg-gray-300 flex flex-col justify-center items-center rounded-md pl-4 pt-12 space-y-12 "
-                  >
-                    <p>Activity: {task.Activity}</p>
-                    <p>Date: {task.Date}</p>
-                    <div className="mt-20 justify-between space-x-4">
-                      <button
-                        className="border-2 border-purple-700 rounded-md w-[3vw] text-purple-900 hover:bg-blue-600 hover:text-white"
-                        onClick={() => handleEdit(index)}
-                      >
-                        Edit
-                      </button>
-                      <button
-                        className="border-2 border-purple-700 rounded-md w-[3vw] text-purple-900 hover:bg-red-600 hover:text-white"
-                        onClick={() => handleDelete(index)}
-                      >
-                        Delete
-                      </button>
-                    </div>
+              <div
+                key={task.Number}
+                className="flex flex-col justify-center items-center"
+              >
+                <div className="ml-4 h-[28vh] w-[12.5vw] bg-gray-300 flex flex-col justify-center items-center rounded-md  space-y-8 ">
+                  <p>Activity: {task.Activity}</p>
+                  <p>Date: {task.Date}</p>
+                  <div className="mt-20 justify-between space-x-4">
+                    <button
+                      className="border-2 border-purple-700 rounded-md w-[3vw] text-purple-900 hover:bg-blue-600 hover:text-white"
+                      onClick={() => handleEdit(task.Number)}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      className="border-2 border-purple-700 rounded-md w-[3vw] text-purple-900 hover:bg-red-600 hover:text-white"
+                      onClick={() => handleDelete(index)}
+                    >
+                      Delete
+                    </button>
                   </div>
                 </div>
-              </>
+              </div>
             ))}
           </div>
         )}
-        <Modal
-          showModal={showModal}
-          handleModalClose={handleModalClose}
-          handleAttList={handleAttList}
-        />
+        {showModal && (
+          <Modal
+            handleModalClose={handleModalClose}
+            handleAttList={handleAttList}
+            handleEdit={handleEdit}
+          />
+        )}
       </div>
     </div>
   );
--- ToDoListLocalStorage/src/Components/Modal.jsx
+++ ToDoListLocalStorage/src/Components/Modal.jsx
@@ -1,9 +1,7 @@
 import { useState } from "react";
 
-export default function Modal({ showModal, handleModalClose, handleAttList }) {
+export default function Modal({ handleModalClose, handleAttList }) {
   const [task, setTask] = useState({});
-
-  if (!showModal) return null;
 
   const handleSaveTask = () => {
     handleAttList(task);
@@ -18,8 +16,8 @@ export default function Modal({ showModal, handleModalClose, handleAttList }) {
   return (
     <div className=" justify-center items-center bg-black bg-opacity-25 backdrop-blur-sm flex fixed inset-0 w-[100vw] h-[100vh]">
       <div className="w-[30vw] h-[35vh] ">
-        <div className="bg-gray-100 p2 rounded h-[35vh]">
-          <div className="flex justify-end p-4 w-[30vw] ">
+        <div className="bg-gray-100 rounded h-[35vh]">
+          <div className="flex justify-end p-2 w-[30vw] ">
             <button
               className="justify-center items-center flex bg-red-600 text-gray-300 border-2 border-black w-6 h-6 hover:bg-gray-100 hover:text-red-600"
               onClick={handleCancelTask}
@@ -27,7 +25,7 @@ export default function Modal({ showModal, handleModalClose, handleAttList }) {
               X
             </button>
           </div>
-          <div className="flex-col justify-center items-center flex space-y-8 pl-4">
+          <div className="flex-col justify-center items-center flex space-y-2 ">
             <p>Type your activity: </p>
             <input
               label="Activity: "
@@ -41,7 +39,7 @@ export default function Modal({ showModal, handleModalClose, handleAttList }) {
               onChange={(e) => setTask({ ...task, Date: e.target.value })}
             />
           </div>
-          <div className="justify-center items-center flex pt-8">
+          <div className="justify-center items-center flex pt-6">
             <button
               className="justify-center items-center flex bg-blue-600 text-gray-100 border-2 border-black w-16 h-6 hover:bg-gray-100 hover:text-blue-600"
               onClick={handleSaveTask}
