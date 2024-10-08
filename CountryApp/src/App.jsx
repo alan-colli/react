39b01d9c085f107373ad55ac2225e77ee1ab1d8b
Alan Colli <150622900+alan-colli@users.noreply.github.com>
2024-10-08 08:26:09 -0300

    att

--- CountryApp/src/App.jsx
+++ CountryApp/src/App.jsx
@@ -17,18 +17,87 @@ export default function App() {
     setShowModal(false);
   };
 
-  const apiEndPoints = [
-    "https://restcountries.com/v3.1/name/germany",
-    "https://restcountries.com/v3.1/name/france",
-    "https://restcountries.com/v3.1/name/netherlands",
-    "https://restcountries.com/v3.1/name/russia",
-    "https://restcountries.com/v3.1/name/japan",
-    "https://restcountries.com/v3.1/name/united%20kingdom",
-    "https://restcountries.com/v3.1/name/canada",
-    "https://restcountries.com/v3.1/name/united%20states",
-    "https://restcountries.com/v3.1/name/argentina",
-    "https://restcountries.com/v3.1/name/brazil",
-  ];
+  const handleBrazil = () => {
+    axios
+      .get("https://restcountries.com/v3.1/name/brazil")
+      .then((res) => {
+        const countryDataBrazil = res.data[0];
+        setSelectedCountry(countryDataBrazil);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  axios
+    .get("https://restcountries.com/v3.1/name/argentina")
+    .then((res) => {
+      const countryDataArg = res.data[0];
+      setSelectedCountry(countryDataArg);
+    })
+    .catch((err) => console.log(err));
+
+  axios
+    .get("https://restcountries.com/v3.1/name/united%20states")
+    .then((res) => {
+      const countryDataUSA = res.data[0];
+      setSelectedCountry(countryDataUSA);
+    })
+    .catch((err) => console.log(err));
+
+  axios
+    .get("https://restcountries.com/v3.1/name/canada")
+    .then((res) => {
+      const countryDataCanada = res.data[0];
+      setSelectedCountry(countryDataCanada);
+    })
+    .catch((err) => console.log(err));
+
+  axios
+    .get("https://restcountries.com/v3.1/name/united%20kingdom")
+    .then((res) => {
+      const countryDataUK = res.data[0];
+      setSelectedCountry(countryDataUK);
+    })
+    .catch((err) => console.log(err));
+
+  axios
+    .get("https://restcountries.com/v3.1/name/japan")
+    .then((res) => {
+      const countryDataJapan = res.data[0];
+      setSelectedCountry(countryDataJapan);
+    })
+    .catch((err) => console.log(err));
+
+  axios
+    .get("https://restcountries.com/v3.1/name/russia")
+    .then((res) => {
+      const countryDataRussia = res.data[0];
+      setSelectedCountry(countryDataRussia);
+    })
+    .catch((err) => console.log(err));
+
+  axios
+    .get("https://restcountries.com/v3.1/name/netherlands")
+    .then((res) => {
+      const countryDataNetherlands = res.data[0];
+      setSelectedCountry(countryDataNetherlands);
+    })
+    .catch((err) => console.log(err));
+
+  axios
+    .get("https://restcountries.com/v3.1/name/france")
+    .then((res) => {
+      const countryDataFrance = res.data[0];
+      setSelectedCountry(countryDataFrance);
+    })
+    .catch((err) => console.log(err));
+
+  axios
+    .get("https://restcountries.com/v3.1/name/germany")
+    .then((res) => {
+      const countryDataGermany = res.data[0];
+      setSelectedCountry(countryDataGermany);
+    })
+    .catch((err) => console.log(err));
 
   return (
     <div className="bg-blue-900 h-[100vh] flex flex-col items-center pt-12 ">
