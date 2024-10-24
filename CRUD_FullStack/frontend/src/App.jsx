1d73d63f8c4558b71aca4269ee5aea9df06635a5
Alan Colli <150622900+alan-colli@users.noreply.github.com>
2024-10-24 17:45:44 -0300

    addind front end

---
+++ CRUD_FullStack/frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="bg-gray-300 h-[100vh] w-[100vw]">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ CRUD_FullStack/frontend/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <h1 className="flex justify-center items-center bg-blue-900 text-white ">
+      Insert your users
+    </h1>
+  );
+}
