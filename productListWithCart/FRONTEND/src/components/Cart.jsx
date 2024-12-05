e6d8231b1b405c33c6a00ca7c93583bbda7b1ec0
Alan Colli <150622900+alan-colli@users.noreply.github.com>
2024-12-05 12:56:42 -0300

    a

--- productListWithCart/FRONTEND/src/components/Cart.jsx
+++
@@ -1,3 +0,0 @@
-export default function Cart() {
-  return <div className="bg-red-400 w-[40vw] h-[85vh]"></div>;
-}
--- productListWithCart/FRONTEND/src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <p className="h-[15vh] flex justify-start items-end ml-16 text-4xl">
-      Desserts
-    </p>
-  );
-}
