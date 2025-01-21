8775ef40eff68386a189cb5e1177363aa8e30f44
Alan Colli <150622900+alan-colli@users.noreply.github.com>
2025-01-21 15:50:15 -0300

    att

---
+++ Portfolio/src/components/Header.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="fixed top-0 left-0 w-full bg-gray-950 shadow-md h-[10vh] flex items-center justify-center">
+      <nav className="flex justify-center p-4">
+        <ul className="flex gap-4">
+          <li>
+            <Link
+              to="/"
+              className="text-gray-100  hover:text-gray-600 text-xl "
+            >
+              Home
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              to="/projects"
+              className="text-gray-100 hover:text-gray-500 text-xl"
+            >
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/contact"
+              className="text-gray-100 hover:text-gray-500 text-xl"
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ Portfolio/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="bg-black w-screen h-screen flex justify-center items-center flex-col">
+      <section className="text-gray-100">
+        <h1 className="text-4xl font-bold ">Hey,</h1>
+        <h2 className="text-3xl font-bold ">I'm Alan!</h2>
+        <p className="text-xl">Web developer, focused on Front-End.🚀</p>
+      </section>
+    </div>
+  );
+}
