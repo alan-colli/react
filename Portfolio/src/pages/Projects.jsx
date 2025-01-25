e0f2c1a93c6838ebef7a913cd7a5dc45c0c5db36
Alan Colli <150622900+alan-colli@users.noreply.github.com>
2025-01-25 17:55:54 -0300

    att

--- Portfolio/src/pages/Projects.jsx
+++ Portfolio/src/pages/Projects.jsx
@@ -4,9 +4,15 @@ export default function Projects() {
   return (
     <div className="w-[100vw] h-[100vh] bg-black flex flex-col justify-center  items-center text-white text-3xl ">
       <div className="flex flex-col space-y-12">
-        <Link to="/projects/route1">Countries App</Link>
-        <Link to="/projects/route2">Rota 2</Link>
-        <Link to="/projects/route3">Rota 3</Link>
+        <Link to="/projects/route1" className="hover:scale-110">
+          Countries App
+        </Link>
+        <Link to="/projects/route2" className="hover:scale-110">
+          Contact Manager
+        </Link>
+        <Link to="/projects/route3" className="hover:scale-110">
+          Rota 3
+        </Link>
       </div>
     </div>
   );
--- Portfolio/src/pages/projects-pages/Project2.jsx
+++ Portfolio/src/pages/projects-pages/Project2.jsx
@@ -1,5 +1,11 @@
 import { Link } from "react-router-dom";
 import ArrowImage from "../../figures/iconmonstr-arrow-left-alt-filled-240.png";
+import AddingComp from "../../figures/project2/AddingComp.gif";
+import AddingMob from "../../figures/project2/AddingMob.gif";
+import DeletingMob from "../../figures/project2/DeletingMob.gif";
+import MobImg from "../../figures/project2/MobImg.png";
+import SearchMob from "../../figures/project2/SearchMob.gif";
+import UpdatingMob from "../../figures/project2/UpdatingMob.gif";
 
 function Route2() {
   return (
@@ -26,8 +32,77 @@ function Route2() {
           As helper tools, I used DBeaver to manage the database and Insomnia to
           monitor the API requests.
         </p>
+        <div className="mb-6">
+          <p className="text-lg sm:text-xl mb-2">
+            Here it's how the application looks:
+          </p>
+          <div className="relative">
+            <img
+              id="gif"
+              src={MobImg}
+              alt="App Animation"
+              className="w-full rounded-lg shadow-md transition-opacity duration-300"
+            />
+          </div>
+        </div>
+        <div className="mb-6">
+          <p className="text-lg sm:text-xl mb-2">Creating a contact:</p>
+          <div className="relative">
+            <img
+              id="gif"
+              src={AddingMob}
+              alt="App Animation"
+              className="w-full rounded-lg shadow-md transition-opacity duration-300"
+            />
+          </div>
+          <div className="mb-6">
+            <p className="text-lg sm:text-xl mb-2">Updating a contact:</p>
+            <div className="relative">
+              <img
+                id="gif"
+                src={UpdatingMob}
+                alt="App Animation"
+                className="w-full rounded-lg shadow-md transition-opacity duration-300"
+              />
+            </div>
+          </div>
+          <div className="mb-6">
+            <p className="text-lg sm:text-xl mb-2">deleting a contact:</p>
+            <div className="relative">
+              <img
+                id="gif"
+                src={DeletingMob}
+                alt="App Animation"
+                className="w-full rounded-lg shadow-md transition-opacity duration-300"
+              />
+            </div>
+          </div>
+          <div className="mb-6">
+            <p className="text-lg sm:text-xl mb-2">Searching a contact:</p>
+            <div className="relative">
+              <img
+                id="gif"
+                src={SearchMob}
+                alt="App Animation"
+                className="w-full rounded-lg shadow-md transition-opacity duration-300"
+              />
+            </div>
+          </div>
+          <div className="mb-6">
+            <p className="text-lg sm:text-xl mb-2">
+              How it looks on bigger screens:
+            </p>
+            <div className="relative">
+              <img
+                id="gif"
+                src={AddingComp}
+                alt="App Animation"
+                className="w-full rounded-lg shadow-md transition-opacity duration-300"
+              />
+            </div>
+          </div>
+        </div>
       </div>
-      <div></div>
     </div>
   );
 }
