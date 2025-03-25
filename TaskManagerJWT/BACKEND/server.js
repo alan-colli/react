18574706e94606c4f86dcf3938d5c7fd30740a83
Alan Colli <150622900+alan-colli@users.noreply.github.com>
2025-03-25 10:18:38 -0300

    att

---
+++ TaskManagerJWT/BACKEND/server.js
@@ -0,0 +1,14 @@
+import { configDotenv } from "dotenv";
+import express from "express";
+
+configDotenv();
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Working on ${PORT}`);
+});
